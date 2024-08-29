e6c2b060dde4f6a3f691c1167b678e8028040473
Adwiteek <sadwiteek@gmail.com>
2024-08-30 01:04:15 +0530

    This is a repo for the template and snippets that I use
    Templates and Snippets really helps me a lot in CP by saving a lot of time. Here you can find all the templates and snippets that I use while coding a problem
    
    Co-Authored-By: Adwiteek <150584084+advtik@users.noreply.github.com>

---
+++ main_template.cpp
@@ -0,0 +1,50 @@
+//The best thing about time is that it changes
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+#define testcases ll tsts; cin>>tsts; for(ll t=0;t<tsts;t++)
+#define MOD 1000000007
+#define MOD1 998244353
+#define endl "\n"
+#define pb push_back
+#define ppb pop_back
+#define PI 3.141592653589793238462
+#define ff first
+#define ss second
+//for rotatory array you can use mod of index by size of the array
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+
+
+#ifndef ONLINE_JUDGE
+#include "Adibug.h"
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+#ifndef ONLINE_JUDGE
+#include "Adibug2.h"
+#define deb(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define deb(x...)
+#endif
+void solve(){
+   
+}
+int main(){
+fastio
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("Error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+    testcases{
+        solve();
+    }
+    return 0;
+}
